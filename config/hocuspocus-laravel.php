aef68aef384296dec4638894ff236665690746d1
Dushan <dushancimbalevic@gmail.com>
2025-04-15 16:00:15 +0200

    port added in config file

--- config/hocuspocus-laravel.php
+++ config/hocuspocus-laravel.php
@@ -2,32 +2,17 @@
 
 return [
 
-    /*
-     *
-     */
     'route' => '/api/documents',
-
-    /*
-     *
-     */
+    'port' => env('HOCUSPOCUS_PORT', 1234),
+    'job_connection' => env('HOCUSPOCUS_LARAVEL_JOB_CONNECTION'),
+    'job_queue' => env('HOCUSPOCUS_LARAVEL_JOB_QUEUE'),
+    'secret' => env('HOCUSPOCUS_SECRET', ''),
+    'access_token_parameter' => 'access_token',
     'events' => [
         \Hocuspocus\HocuspocusLaravel::EVENT_ON_CHANGE,
         \Hocuspocus\HocuspocusLaravel::EVENT_ON_CONNECT,
         \Hocuspocus\HocuspocusLaravel::EVENT_ON_DISCONNECT,
         \Hocuspocus\HocuspocusLaravel::EVENT_ON_CREATE_DOCUMENT,
     ],
-
-    'job_connection' => env('HOCUSPOCUS_LARAVEL_JOB_CONNECTION'),
-    'job_queue' => env('HOCUSPOCUS_LARAVEL_JOB_QUEUE'),
-
-    /*
-     *
-     */
-    'secret' => env('HOCUSPOCUS_SECRET', ''),
-
-    /*
-     *
-     */
-    'access_token_parameter' => 'access_token',
 
 ];
